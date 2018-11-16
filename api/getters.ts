039ae66f598cd6eb713ca035b86f21b96d6ad81c
Maikel van Veen <maikeloo10@hotmail.com>
2018-11-16 14:00:58 +0100

    fix(getters): comment unused code

--- api/getters.ts
+++ api/getters.ts
@@ -1,61 +1,49 @@
-import { Result } from './types/Query'
+// import { Result } from './types/Query'
 
-export const getTransformedCityData = (cityData: string) => {
-    return cityData
-        .split(/\n/g)
-        .map(city => city.split(/\,/g))
-        .map(city => ({
-            countryCode: city[0],
-            cityName: city[2],
-            latitude: city[5],
-            longitude: city[6],
-        }))
-}
+// export const getLanguageFromResult = (result?: Result): string => {
+//     return result.languages
+//         && result.languages.language
+//         && result.languages.language.$t
+//         || undefined
+// }
 
-export const getLanguageFromResult = (result?: Result): string => {
-    return result.languages
-        && result.languages.language
-        && result.languages.language.$t
-        || undefined
-}
+// export const getAuthorFromResult = (result?: Result): string => {
+//     return result.authors
+//         && result.authors.author
+//         && result.authors.author.$t
+//         || undefined
+// }
 
-export const getAuthorFromResult = (result?: Result): string => {
-    return result.authors
-        && result.authors.author
-        && result.authors.author.$t
-        || undefined
-}
+// export const getGenreFromResult = (result?: Result): string | string[] => {
+//     const genres = result.genres
 
-export const getGenreFromResult = (result?: Result): string | string[] => {
-    const genres = result.genres
+//     if (genres && genres.genre) {
+//         const genre = genres.genre
 
-    if (genres && genres.genre) {
-        const genre = genres.genre
+//         if (Array.isArray(genre)) {
+//             return genre.map(genre => genre.$t)
+//         } else {
+//             return genre.$t
+//         }
+//     }
 
-        if (Array.isArray(genre)) {
-            return genre.map(genre => genre.$t)
-        } else {
-            return genre.$t
-        }
-    }
+//     return undefined
+// }
 
-    return undefined
-}
+// export const getTitleFromResult = (result?: Result): string => {
+//     return result.titles
+//         && result.titles['short-title']
+//         && result.titles['short-title'].$t
+//         || undefined
+// }
 
-export const getTitleFromResult = (result?: Result): string => {
-    return result.titles
-        && result.titles['short-title']
-        && result.titles['short-title'].$t
-        || undefined
-}
-
-export const getYearOfPublicationFromResult = (result?: Result): number => {
-    return result.publication
-        && result.publication.year
-        && result.publication.year.$t
-        && Number(result.publication.year.$t)
-        || undefined
-}
+// export const getYearOfPublicationFromResult = (result?: Result): number => {
+//     return result.publication
+//         && result.publication.year
+//         && result.publication.year.$t
+//         && Number(result.publication.year.$t)
+//         || undefined
+// }
 
 const getCleanPublicationLocation = (publicationLocation: string) => {
     return publicationLocation
