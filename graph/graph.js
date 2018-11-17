3ddd70d785d782c812315782a8e2d8065a079d62
Maikel van Veen <maikeloo10@hotmail.com>
2018-11-17 14:42:33 +0100

    feat(graph): add types

--- graph/graph.js
+++ graph/graph.js
@@ -1,7 +1,5 @@
 // MapBox //
 
-const view = "map"
-
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFpa3h4IiwiYSI6ImNqb2p0b2c1ZzA4NWIzdnBhNTJhMjk3MmIifQ.WWh5GShedNrj-2eYYnkXxw'
 
 const map = new mapboxgl.Map({
@@ -12,32 +10,53 @@ const map = new mapboxgl.Map({
 })
 
 // Binding D3 and MapBox //
-
 const canvas = map.getCanvasContainer()
-
 const svg = d3.select(canvas).append("svg")
-
-map.on('load', async () => {
-    const data = await d3.json("//api.jsonbin.io/b/5bf00a8518a56238b6f7c928/4")
-    drawData(data)
-})
-
 const project = d => map.project(new mapboxgl.LngLat(+d[0], +d[1]))
 
 // D3 //
 
-// Draw GeoJSON data with d3
+/**
+* @param {Number} transitionTime
+*/
+const update = (transitionTime = 0) => {
+    svg.selectAll("circle")
+        .transition()
+        .duration(transitionTime)
+            .attr("cx", d => project(d.geometry.coordinates).x )
+            .attr("cy", d => project(d.geometry.coordinates).y )
+}
+
+/**
+* @param {Object} data
+* @param {String} data.type
+* @param {Object} data.properties
+* @param {String} data.properties.name
+* @param {String} data.properties.dataClass
+* @param {String[]} data.properties.books
+* @param {Object} data.geometry
+* @param {String} data.geometry.type
+* @param {Array} data.geometry.coordinates
+* @param {Number} data.geometry.coordinates[].lon
+* @param {Number} data.geometry.coordinates[].lat
+*/
+const getStyleClassFromDataClass = data => {
+    return data.properties.dataClass === 'main'
+        ? 'circle--main'
+        : 'circle'
+}
+
 let circles
-function drawData(data) {
-    // Add circles
+
+const drawData = data => {
     circles = svg.selectAll("circle")
         .data(data.features)
         .enter()
         .append("circle")
             .attr("r", 8)
-            .attr("class", d => d.properties.dataClass === 'main' ? 'circle--main' : 'circle')
-            .on("click", (d) => {
-                // Show all the circles
+            .attr("class", getStyleClassFromDataClass)
+            .on("click", d => {
+                // Think of something to show all the names of the books
                 alert(d.properties.name)
             })
 
@@ -45,63 +64,14 @@ function drawData(data) {
     update()
 
     // Update on map interaction
-    map.on("viewreset", () => update(0))
-    map.on("move", () => update(0))
-    map.on("moveend", () => update(0))
+    map.on("viewreset", () => update())
+    map.on("move", () => update())
+    map.on("moveend", () => update())
 }
 
-// Update function
-function update(transitionTime) {
-    // Default value = 0
-    transitionTime = typeof transitionTime !== 'undefined'
-        ? transitionTime
-        : 0
+map.on('load', async () => {
+    const data = await d3.json("//api.jsonbin.io/b/5bf00a8518a56238b6f7c928/4")
 
-    // Map view
-    if (view === "map") {
-        svg.selectAll("circle")
-            .transition()
-            .duration(transitionTime)
-                .attr("cx", (d) => project(d.geometry.coordinates).x )
-                .attr("cy", (d) => project(d.geometry.coordinates).y )
+    drawData(data)
+})
 
-    // Grid view
-    } else if (view === "grid") {
-        let ix = 0
-        let iy = 0
-        let rows = 3
-        let cols = 3
-
-        // Check window with and height
-        const windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-        const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-
-        const gridItemWidth = (windowWidth * 0.8) / (cols + 1)
-        const gridItemHeight = windowHeight / (rows + 1)
-
-        svg.selectAll("circle")
-            .each(function(d) {
-
-                let circle = d3.select(this)
-
-                console.log("ix: " + ix + ", iy: " + iy)
-
-                circle
-                    .transition()
-                    .duration(transitionTime)
-                        .attr("cx", d => (windowWidth * 0.2) + (ix * gridItemWidth) + (0.5 * gridItemWidth))
-                        .attr("cy", d => (iy * gridItemHeight) + gridItemHeight)
-
-                // Increase iterators
-                ix++
-                if (ix === cols) {
-                    ix = 0
-                    iy++
-                }
-
-                if (iy === rows) {
-                    iy = 0
-                }
-            })
-    }
-}
