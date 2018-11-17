90cf4cb134d697103a01689f2b767159619a050a
Maikel van Veen <maikeloo10@hotmail.com>
2018-11-17 21:16:34 +0100

    feat(types): add type for queries

--- api/queries.ts
+++ api/queries.ts
@@ -1,9 +1,12 @@
 import * as fetch from 'node-fetch'
+import { BooksByLocation } from './types/Book'
+import { LocationIQPlace } from './types/Location'
 
-export const sendRequest = async location => {
-    const data = await fetch(`https://eu1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&city=${location.key}&format=json`, {
-        method: 'GET',
-    })
+export const sendRequest = async (location: BooksByLocation): Promise<LocationIQPlace> => {
+    const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&city=${location.key}&format=json`
+    const options = { method: 'GET' }
+
+    const data = await fetch(url, options)
 
     return data.json()
 }
